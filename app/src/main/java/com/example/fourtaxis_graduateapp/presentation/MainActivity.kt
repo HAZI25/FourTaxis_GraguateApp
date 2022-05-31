3303c18b237fe5afe8eb48a39f12a711e26372e4
marsel <marsel.nazirov@mail.ru>
2022-05-31 12:23:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fourtaxis_graduateapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fourtaxis_graduateapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fourtaxis_graduateapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
